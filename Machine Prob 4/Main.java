c46d55af0b109b73b22771aee466bfb9b4273d8a
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2023-05-08 12:00:37 +0800

    Create Main.java

---
+++ Machine Prob 4/Main.java
@@ -0,0 +1,10 @@
+       //Circle
+    // //Circle c1 = new Circle(5.5);
+    // System.out.println("Radius:" + c1.getRadius());
+    // System.out.println("Area: " + c1.calculateArea());
+    // System.out.println("Circumference: " + c1.calculateCircumference());
+
+    // c1.setRadius(7.2);
+    // System.out.println("Updated Radius:" + c1.getRadius());
+    // System.out.println("Updated Area: " + c1.calculateArea());
+    // System.out.println("Updated Circumference: " + c1.calculateCircumference());
