5f909f2ffb08b9f4dc35e47d09fcc2f88e3def17
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2023-05-08 10:12:07 +0800

    Create Tao.java

---
+++ Machine Prob 1/Tao.java
@@ -0,0 +1,19 @@
+public class Tao{
+
+    private String pangalan;
+    private int edad;
+    
+   public Tao(String pangalan, int edad){
+    this.pangalan = pangalan;
+    this.edad = edad;
+
+    }
+    public String getName(){
+        return pangalan;
+    }
+    public int getAge(){
+        return edad;
+    }
+    
+    
+}
